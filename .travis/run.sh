6d55c1fe58c676352e897c0211488bc8020e25f4
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-08 19:53:08 +0200

    fix upload url

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 #conan create . Latios96/stable
-export UPLOAD_URL=https://api.bintray.com/conan/latios96/my_conan
+export CONAN_UPLOAD=https://api.bintray.com/conan/latios96/my_conan
 python3 build.py
