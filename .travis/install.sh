4762ba056e7376017aa21d89db6e5be03f498828
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-10-08 10:17:29 +0200

    use docker

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,13 +9,11 @@ pip3 install conan_package_tools
 
 conan user
 
-conan profile show default
-if [ $? -eq 0 ]
-then
+conanDefaultProfile=~/.conan/profiles/default
+if [ -f "$conanDefaultProfile" ]; then
   echo "Conan default profile already exists!"
 else
   echo "Conan default profile not exists, creating.."
-  conan profile new default --detect
 fi
 
 if [[ "$(uname -s)" != 'Darwin' ]]; then
