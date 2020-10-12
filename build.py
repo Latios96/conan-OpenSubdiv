7be1cbd709e52c541d45e118b490dd28f867bdcf
Jan <jan.honsbrok@gmail.com>
2020-10-12 21:23:06 +0200

    disable unsupported

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
+import sys
+
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
+    builder.remove_build_if(
+        lambda build: build.options["OpenSubdiv:shared"] == True and sys.platform == "win32")
     builder.run()
