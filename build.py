2ac27953b7e856882c45bc4bdd21206203aacf7a
Jan <jan.honsbrok@gmail.com>
2020-10-13 10:04:37 +0200

    fixing build.py

--- build.py
+++ build.py
@@ -2,11 +2,18 @@ import sys
 
 from cpt.packager import ConanMultiPackager
 
+
+def skip_if_windows(build):
+    if sys.platform != "win32":
+        return False
+    return build.settings['build_type'] and build.settings[
+        'compiler.runtime'].startswith("MT")
+
+
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     builder.add_common_builds()
     builder.build_policy = "missing"
     builder.remove_build_if(
-        lambda build: (build.options["OpenSubdiv:shared"] == True or build.settings[
-            "compiler.runtime"] == "MT") and sys.platform == "win32")
+        lambda build: build.options["OpenSubdiv:shared"] == True or skip_if_windows(build))
     builder.run()
