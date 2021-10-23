be98e1fcb8570c411a516a0d886c9e26b5515b93
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-10-23 16:57:05 +0200

    dont build with opengl on windows

--- conanfile.py
+++ conanfile.py
@@ -40,18 +40,16 @@ class OpenSubdivConan(ConanFile):
         )
         cmake = CMake(self)
         cmake.definitions["NO_TBB"] = not self.options.with_tbb
+        cmake.definitions["NO_OPENGL"] = self.options.no_opengl
         cmake.definitions["NO_EXAMPLES"] = True
         cmake.definitions["NO_TUTORIALS"] = True
         cmake.definitions["NO_REGRESSION"] = True
-
-        cmake.definitions["NO_TBB"] = True
         cmake.definitions["NO_OMP"] = True
         cmake.definitions["NO_CUDA"] = True
         cmake.definitions["NO_PTEX"] = True
         cmake.definitions["NO_DOC"] = True
         cmake.definitions["NO_CLEW"] = True
         cmake.definitions["NO_OPENCL"] = True
-        cmake.definitions["NO_OPENGL"] = self.options.no_opengl
 
         cmake.configure(
             source_folder="OpenSubdiv-{}".format(self.version_with_underscore)
