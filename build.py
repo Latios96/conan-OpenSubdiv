cfe3b3a5bc9627663fc692f0b7ec44d44a39b3f7
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-10-23 16:42:36 +0200

    fix apple clang version

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ def skip_if_windows(build):
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"],visual_versions=[16], gcc_versions=[9], apple_clang_versions=[12])
+    builder = ConanMultiPackager(archs=["x86_64"],visual_versions=[16], gcc_versions=[9], apple_clang_versions=[12.0])
     builder.add_common_builds()
     builder.build_policy = "missing"
     builder.remove_build_if(lambda build: skip_if_windows(build))
