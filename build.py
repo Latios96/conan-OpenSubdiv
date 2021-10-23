572a48bc1a25ee8738e95724c54923a0fc8d27af
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-10-23 16:28:55 +0200

    not all visual studio versions

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ def skip_if_windows(build):
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"])
+    builder = ConanMultiPackager(archs=["x86_64"],visual_versions=[16])
     builder.add_common_builds()
     builder.build_policy = "missing"
     builder.remove_build_if(
